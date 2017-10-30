23e79bc851c06d62eb50a9670ae65cbf137afa6d
Bastien CADIOT <bastien.cadiot@gmail.com>
2017-10-30 09:15:31 +0100

    Update e02 outputs to expose traefik public ips

--- e02_services_lb_layer/aws_outputs.tf
+++ e02_services_lb_layer/aws_outputs.tf
@@ -2,11 +2,12 @@ output "aws_consul_ips" {
   value = ["${aws_instance.consul.*.private_ip}"]
 }
 
-output "aws_traefik_ips" {
-  value = [
-      "${aws_instance.traefik.*.public_ip}",
-      "${aws_instance.traefik.*.private_ip}"
-    ]
+output "aws_traefik_private_ips" {
+  value = ["${aws_instance.traefik.*.private_ip}"]
+}
+
+output "aws_traefik_public_ips" {
+  value = ["${aws_instance.traefik.*.public_ip}"]
 }
 
 output "aws_consul_client_sg" {
--- e02_services_lb_layer/gcp_outputs.tf
+++ e02_services_lb_layer/gcp_outputs.tf
@@ -2,9 +2,10 @@ output "gcp_consul_ips" {
   value = ["${google_compute_instance.consul.*.network_interface.0.address}"]
 }
 
-output "gcp_traefik_ips" {
-  value = [
-      "${google_compute_instance.traefik.*.network_interface.0.access_config.0.assigned_nat_ip}",
-      "${google_compute_instance.traefik.*.network_interface.0.address}"
-    ]
+output "gcp_traefik_public_ips" {
+  value = ["${google_compute_instance.traefik.*.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+output "gcp_traefik_private_ips" {
+  value = ["${google_compute_instance.traefik.*.network_interface.0.address}"]
 }
