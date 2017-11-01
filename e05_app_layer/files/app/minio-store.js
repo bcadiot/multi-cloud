665b9226c472bab916c42c1c4b80110fed149100
Bastien CADIOT <bastien.cadiot@gmail.com>
2017-11-01 13:04:02 +0100

    Add demo app

---
+++ e05_app_layer/files/app/minio-store.js
@@ -0,0 +1,86 @@
+/*
+ * Store Application, (C) 2016 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var express = require('express');
+var app = express();
+var Minio = require('minio');
+
+// Set up handlebars view engine.
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
+
+var minio_external_host = process.env.MINIO_EXTERNAL_HOST || 'localhost'
+var minio_external_port = process.env.MINIO_EXTERNAL_PORT || 9000
+
+// Instantiate a minioClient Object with an endPoint, port & keys.
+var minioClient = new Minio.Client({
+  endPoint: process.env.MINIO_HOST || 'localhost',
+  port: parseInt(process.env.MINIO_PORT, 10) || 9000,
+  secure: false,
+  accessKey: process.env.MINIO_ACCESS_KEY || 'EQHUWYGN3WHYZ79716E4',
+  secretKey: process.env.MINIO_SECRET_KEY || '6UFSFT8QGQuF9ndefAyVsM9DjkDVyyUFM1mJMNkd'
+});
+
+var minioBucket = 'minio-store'
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+  var assets = [];
+  var objectsStream = minioClient.listObjects(minioBucket, '', true)
+  objectsStream.on('data', function(obj) {
+    console.log(obj);
+    // Lets construct the URL with our object name.
+    var publicUrl = minioClient.protocol + '//' + minio_external_host + ':' + minio_external_port + '/' + minioBucket + '/' + obj.name
+    assets.push(publicUrl);
+  });
+  objectsStream.on('error', function(e) {
+    console.log(e);
+  });
+  objectsStream.on('end', function(e) {
+    console.log(assets);
+    // Pass our assets array to our home.handlebars template.
+    res.render('home', { url: assets });
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.set('port', process.env.PORT || 3000);
+// Custom 404 page.
+app.use(function(req, res) {
+  res.type('text/plain');
+  res.status(404);
+  res.render('404');
+});
+
+// Custom 500 page.
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.type('text/plain');
+  res.render('500');
+});
+
+// Start the app.
+app.listen(app.get('port'), '0.0.0.0', function(){
+  console.log('Express started on http://0.0.0.0:' +
+              app.get('port') + '; press Ctrl-C to terminate.' );
+});
