d4edd4efb1d94036502f494e28d51d767184a679
Bastien CADIOT <bastien.cadiot@gmail.com>
2017-11-23 10:50:41 +0100

    Add wrapper scripts

---
+++ create_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export TF_VAR_gcp_instance_type='n1-standard-1'
+export TF_VAR_aws_instance_type='t2.small'
+
+cd e01_network_layer/
+terraform apply
+cd ../e02_services_lb_layer/
+terraform apply
+cd ../e03_scheduler_layer/
+terraform apply
+cd ../e04_data_layer/
+terraform apply
+cd ../e05_app_layer/
+terraform apply
